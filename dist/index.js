aab53f85fbb2f23f392618c85b80c1ed5018c784
yuuthod <yuuthod@gmail.com>
2019-04-16 22:04:02 +0900

    typescript 2

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "yuuthod",
+    age: 15,
+    gender: "women"
+};
+const sayHi = (person) => {
+    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
+};
+console.log(sayHi(person));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+
+interface Human {
+    name:string;
+    age:number;
+    gender:string;
+}
+
+const person = {
+    name: "yuuthod",
+    age: 15,
+    gender: "women"
+}
+
+const sayHi = (person: Human):void => {
+    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
+}
+
+console.log(sayHi(person));
+
+export {};
