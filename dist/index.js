b0ae69d6884269398ee9407d8c37f39ec05a44e4
yuuthod <yuuthod@gmail.com>
2019-04-16 22:21:20 +0900

    typescript 3

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const gildong = new Human("gildong", 25, "women");
 const person = {
     name: "yuuthod",
     age: 15,
@@ -8,5 +16,5 @@ const person = {
 const sayHi = (person) => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 };
-console.log(sayHi(person));
+console.log(sayHi(gildong));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-
-interface Human {
-    name:string;
-    age:number;
-    gender:string;
+class Human {
+    public name:string;
+    public age:number;
+    public gender:string;
+    constructor(name:string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
+
+const gildong = new Human("gildong", 25, "women");
 
 const person = {
     name: "yuuthod",
@@ -15,6 +21,6 @@ const sayHi = (person: Human):void => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 }
 
-console.log(sayHi(person));
+console.log(sayHi(gildong));
 
 export {};
