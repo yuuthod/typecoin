9db5554d8c6cc30af8f5e297b27424304054c030
yuuthod <yuuthod@gmail.com>
2019-04-17 10:53:29 +0900

    typechain 2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,7 +10,20 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+// static method : class가 생성되지 않아도 사용할 수 있는 method
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "324234234234", "", "Hello", 333423);
 let blockchain = [genesisBlock];
-console.log(blockchain);
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previosBlock = getLatestBlock();
+    const newIndex = previosBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previosBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number; 
+
+    // static method : class가 생성되지 않아도 사용할 수 있는 method
+    static calculateBlockHash = (
+        index:number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index:number,
         hash:string,
@@ -19,10 +30,38 @@ class Block {
     }
 }
 
+
 const genesisBlock:Block = new Block(0, "324234234234", "", "Hello", 333423);
 
 let blockchain: [Block] = [genesisBlock];
 
-console.log(blockchain); 
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+
+const createNewBlock = (data:string) : Block => {
+    const previosBlock: Block = getLatestBlock();
+    const newIndex : number = previosBlock.index + 1;
+    const newTimestamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(
+        newIndex, 
+        previosBlock.hash,
+        newTimestamp, 
+        data
+    );
+    const newBlock : Block = new Block(
+        newIndex, 
+        newHash, 
+        previosBlock.hash, 
+        data, 
+        newTimestamp
+    );
+    return newBlock;
+}
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 export {};
